6064eb68c0082fd57048b707bd8d0da025932522
mouse0429 <mouse0429@naver.com>
2022-07-05 11:08:08 +0900

    3-12 앵커
    3-12 앵커

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import HttpResponseNotAllowed
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
 from django.utils import timezone
 
 from ..forms import AnswerForm
@@ -18,7 +18,7 @@ def answer_create(request, question_id):
       answer.create_date = timezone.now()
       answer.question = question
       answer.save()
-      return redirect('pybo:detail', question_id=question.id)
+      return redirect('{}#answer_{}'.format(resolve_url('pybo:detail', question_id=question.id), answer.id))
   else:
     return HttpResponseNotAllowed('Only POST is possible.')
   context = {'question': question, 'form': form}
@@ -36,7 +36,7 @@ def answer_modify(request, answer_id):
       answer = form.save(commit=False)
       answer.modify_date = timezone.now()
       answer.save()
-      return redirect('pybo:detail', question_id=answer.question.id)
+      return redirect('{}#answer_{}'.format(resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
 
 @login_required(login_url='common:login')
 def answer_delete(request, answer_id):
@@ -54,4 +54,4 @@ def answer_vote(request, answer_id):
     messages.error(request, '본인이 작성한 글은 추천할 수 없습니다')
   else:
     answer.voter.add(request.user)
-  return redirect('pybo:detail', question_id=answer.question.id)
+  return redirect('{}#answer_{}'.format(resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
