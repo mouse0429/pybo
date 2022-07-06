4ada488148c8c1c1abd5acf765cd860a76a87af0
mouse0429 <mouse0429@naver.com>
2022-07-06 14:38:49 +0900

    추가기능 - 답변 페이징과 정렬
    답변 페이징과 정렬 구현

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,8 +1,8 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.db.models import Q
+from django.db.models import Q, Count
 
-from ..models import Question
+from ..models import Question, Answer
 
 def index(request):
   page = request.GET.get('page', '1')
@@ -24,5 +24,15 @@ def index(request):
 
 def detail(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
-  context = {'question': question}
+  page = request.GET.get('page', '1')
+  order = request.GET.get('order', 'recommended')
+  answer_list = Answer.objects.filter(question_id=question.id).annotate(voter_count=Count('voter')).order_by('-voter_count')
+  if order == 'latest':
+    answer_list = Answer.objects.filter(question_id=question.id).order_by('-create_date')
+  elif order == 'create':
+    answer_list = Answer.objects.filter(question_id=question.id).order_by('create_date')
+  paginator = Paginator(answer_list, 10)
+  max_index = len(paginator.page_range)
+  page_obj = paginator.get_page(page)
+  context = {'question': question, 'answer_list': page_obj, 'max_index': max_index, 'page': page, 'order': order}
   return render(request, 'pybo/question_detail.html', context)
